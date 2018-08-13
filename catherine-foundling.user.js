15d02bc687f88bea160a41f3807c7a48eeb885fa
James Cook <jamescoo@microsoft.com>
2018-08-13 16:41:24 -0700

    Skip last p block

--- catherine-foundling.user.js
+++ catherine-foundling.user.js
@@ -18,14 +18,15 @@
         var content = comment.getElementsByClassName("comment-content")[0];
         var commentParagraphs = content.getElementsByTagName("p");
 
-        for (var paragraph of commentParagraphs) {
+        // skip the last one, which is used for comment likes
+        for (int i = 0; i < commentParagraphs.length - 1; i++) {
+            var paragraph = commentParagraphs[i];
             var text = paragraph.innerText;
             // Some of you actually use "chat" correctly. Only strip out greater forms of Evil.
             text = text.replace(/Chath?e?rine/gi, "Catherine");
             if (author == "daegone823") {
                 // the OG monster, there shall be no chat
                 text = text.replace(/Chat/g, "Cat");
-                console.log(author, text);
             }
             paragraph.innerText = text;
         }
